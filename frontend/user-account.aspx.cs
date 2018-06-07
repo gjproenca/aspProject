5efa726d73d85c952dd43170862707404855dfb2
gjsproenca <gjsproenca@gmail.com>
2018-06-07 17:20:15 +0100

    add files to try solve edit data account

--- frontend/user-account.aspx.cs
+++ frontend/user-account.aspx.cs
@@ -10,17 +10,20 @@ using System.Web.UI.WebControls;
 
 public partial class frontend_user_account : System.Web.UI.Page
 {
+    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        //Response.Write(Session["sessionIDUser"]);
+        Response.Write(Session["sessionIDUser"]);
 
         DataTable dt = new DataTable();
-        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
         conn.Open();
         SqlCommand sqlCmd = new SqlCommand("SELECT * FROM [User] WHERE [IDUser] = @iduser", conn);
         SqlDataAdapter sqlDa = new SqlDataAdapter(sqlCmd);
         sqlCmd.Parameters.AddWithValue("@iduser", Session["sessionIDUser"]);
         sqlDa.Fill(dt);
+
+        Response.Write("page load");
 
         textBoxName.Text = dt.Rows[0]["Name"].ToString();
         textBoxUsername.Text = dt.Rows[0]["Username"].ToString();
@@ -31,37 +34,20 @@ public partial class frontend_user_account : System.Web.UI.Page
 
     protected void submitSignup_Click(object sender, EventArgs e)
     {
+        Response.Write("submit");
+
         //FIXME: Update data
-    //    using (SqlConnection conn = new SqlConnection(configurationManager))
-    //    {
-    //        using (SqlCommand cmd = new SqlCommand("usp_insert_user"))
-    //        {
-    //            using (SqlDataAdapter sda = new SqlDataAdapter())
-    //            {
-    //                cmd.CommandType = CommandType.StoredProcedure;
-    //                cmd.Parameters.AddWithValue("@name", textBoxName.Text);
-    //                cmd.Parameters.AddWithValue("@username", textBoxUsername.Text);
-    //                cmd.Parameters.AddWithValue("@password", textBoxPassword.Text);
-    //                cmd.Parameters.AddWithValue("@email", textBoxEmail.Text);
-    //                cmd.Connection = conn;
-    //                conn.Open();
-    //                userId = Convert.ToInt32(cmd.ExecuteScalar());
-    //                conn.Close();
-    //            }
-    //        }
-    //        switch (userId)
-    //        {
-    //            case -1:
-    //                ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Nome de utilizador já existente!');", true);
-    //                break;
-    //            case -2:
-    //                ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Endereço de e-mail já existente, se quiser recuperar a password clique em Recuperar Senha);", true);
-    //                break;
-    //            default:
-    //                SendEmail(userId);
-    //                Response.Redirect("~/frontend/success.aspx");
-    //                break;
-    //        }
-    //    }
-    //}
+        SqlCommand cmd = new SqlCommand("UPDATE [Table] SET [Name] = @name WHERE [IDUser] = 1", conn);
+        //cmd.Parameters.AddWithValue("@iduser", Session["sessionIDUser"]);
+        cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+        //cmd.Parameters.AddWithValue("@email", textBoxEmail.Text);
+        conn.Open();
+        Response.Write("before execute");
+
+        cmd.ExecuteNonQuery();
+        Response.Write("after execute");
+        conn.Close();
+        Response.Write("after connclose");
+
+    }
 }
